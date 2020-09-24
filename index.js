9c4162c83e1757b7a17e7923035cd0e863f9a079
ron <rinbar70@gmail.com>
2020-09-24 16:10:19 +0300

    Set up a socket.io server and a client page.

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 import express from 'express';
 import morgan from 'morgan';
+import socketio from 'socket.io';
 
 const port = process.env.PORT || '4000';
 
 const app = express();
 
 app.use(morgan('dev'));
+app.use(express.static('public'));
 
-app.listen(port);
+socketio(app.listen(port)).on('connect', function (socket) {
+    socket.on('chat', function (message) {
+        console.log(message);
+    });
+});
+
