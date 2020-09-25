e75b4b4f3eebe9408d865bc33c6ff39fecf9fde6
ron <rinbar70@gmail.com>
2020-09-25 21:57:25 +0300

    Factor out getSqlConnection().

--- dal/message.js
+++ dal/message.js
@@ -1,23 +1,30 @@
 import mysql from 'mysql2/promise';
 
+const getSqlConnection = (function () {
+    let connection = null;
+    return async function () {
+        if (!connection) {
+            connection = await mysql.createConnection({
+                user: process.env['USER'],
+                password: process.env['PASS'],
+                database: 'chat',
+            });
+        }
+        return connection;
+    }
+})();
+
 export async function insertMessage(username, text) {
-    const conn = await mysql.createConnection({
-        user: 'root',
-        database: 'chat',
+    const conn = await getSqlConnection();
+    const [{ insertId: senderId }] = await conn.execute('insert ignore sender (name) values (?)', [username]);
+    const [{ insertId: messageId }] = await conn.execute({
+        sql: `insert message (sender_id, text) values (${senderId ? ':senderId' : '(select id from sender where name = :username)'}, :text)`,
+        namedPlaceholders: true,
+    }, {
+        senderId,
+        username,
+        text,
     });
-    try {
-        const [{ insertId: senderId }] = await conn.execute('insert ignore sender (name) values (?)', [username]);
-        const [{ insertId: messageId }] = await conn.execute({
-            sql: `insert message (sender_id, text) values (${senderId ? ':senderId' : '(select id from sender where name = :username)'}, :text)`,
-            namedPlaceholders: true,
-        }, {
-            senderId,
-            username,
-            text,
-        });
-        console.debug(`(id: ${messageId}, sender_id: ${senderId}, text: '${text}')`);
-    }
-    finally {
-        await conn.end();
-    }
+    console.debug(`(id: ${messageId}, sender_id: ${senderId}, text: '${text}')`);
 }
+
