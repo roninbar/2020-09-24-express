c74f9d3d7bbdd9dfda2f0fd27b55f43d9d9dec1d
ron <rinbar70@gmail.com>
2020-09-25 20:02:08 +0300

    Streamline the SQL.

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ const server = app.listen(port);
 socketio(server).on('connect', function (socket) {
     console.debug(`Client ${socket.client.id} connected.`);
     socket.on('message', async function (message) {
+        const { username, text } = message;
         console.log('message', message);
         socket.broadcast.emit('message', message);
-        const { username, text } = message;
         const conn = await mysql.createConnection({
             user: 'root',
             database: 'chat',
@@ -25,14 +25,12 @@ socketio(server).on('connect', function (socket) {
         try {
             const [{ insertId: senderId }] = await conn.execute('insert ignore sender (name) values (?)', [username]);
             const [{ insertId: messageId }] = await conn.execute({
-                sql: senderId
-                    ? 'insert message (sender_id, text) values (:senderId, :message)'
-                    : 'insert message (sender_id, text) values ((select id from sender where name = :username), :text)',
+                sql: `insert message (sender_id, text) values (${senderId ? ':senderId' : '(select id from sender where name = :username)'}, :text)`,
                 namedPlaceholders: true,
             }, {
                 senderId,
-                text,
                 username,
+                text,
             });
             console.debug(`(id: ${messageId}, sender_id: ${senderId}, text: '${text}')`);
         }
